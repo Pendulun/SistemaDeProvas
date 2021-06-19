67350ee166f621dc67ab7008f1663a7daa2a5c96
Pendulun <danielcampos14@hotmail.com.br>
2021-06-19 17:18:42 -0300

    Criando método config

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include "business/application.hpp"
 
+
+void config(Business::Application* app){
+    //cria um novo app passando uma classe concreta de UserGUI e 
+    //classes concretas de DAO's. Para isso, deve ter um construtor equivalente
+}
+
+
 int main(int argc, char *argv[]){
-    Business::Application app;
-    app.test();
+    Business::Application* app;
+    config(app);
+    app->start();
+    delete app;
     return 0;  
 }
+
