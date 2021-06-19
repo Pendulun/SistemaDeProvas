8aa1a800d05bcc787418e86e9d5e585728679235
Pendulun <danielcampos14@hotmail.com.br>
2021-06-19 17:18:16 -0300

    Adicionando atributo de UserGUI

--- include/business/application.hpp
+++ include/business/application.hpp
@@ -1,11 +1,16 @@
 #ifndef APPLICATION_H
 #define APPLICATION_H
-
+#include "business/gui/UserGUI.hpp"
 #include <iostream>
 namespace Business{
     class Application{
         public:
-            void test();
+            void start();
+            Application(GUI::UserGUI& userGUI);
+            Application();
+            ~Application();
+        private:
+            GUI::UserGUI* userGUI;
     };
 }
 
--- src/business/application.cpp
+++ src/business/application.cpp
@@ -1,7 +1,18 @@
 #include "business/application.hpp"
 
 namespace Business{
-    void Application::test(){
-        std::cout<< "Hello from application!\n";
+
+    void Application::start(){
+        std::cout<<"Starting Application\n";
+    }
+
+    Application::Application(GUI::UserGUI& userGUI){
+        this->userGUI = &userGUI;
+    }
+
+    Application::Application(){}
+
+    Application::~Application(){
+        delete this->userGUI;
     }
 }
