55fd2dd1daced5201a6c678d6d353393967e482a
Pendulun <danielcampos14@hotmail.com.br>
2021-06-24 12:46:45 -0300

    Criando classes ManterUsuario
    Ela vai ter a funcionalidade de cadastro e login de usuário utilizando as classes de persistência. Será utilizada pela GUI

---
+++ include/business/fachadas/manterUsuario.hpp
@@ -0,0 +1,11 @@
+#ifndef __MANTERUSUARIO_H__
+#define __MANTERUSUARIO_H__
+
+namespace Fachadas{
+    class ManterUsuario{
+        public:
+            bool cadastrarUsuario();
+            bool login();
+    };
+}
+#endif
---
+++ src/business/fachadas/manterUsuario.cpp
@@ -0,0 +1,11 @@
+#include "business/fachadas/manterUsuario.hpp"
+
+namespace Fachadas{
+    bool ManterUsuario::cadastrarUsuario(){
+        return true;
+    }
+
+    bool ManterUsuario::login(){
+        return true;
+    }
+}
