8b69d48bada2139e3aef2335701d7fb8b77b3a41
leticiasma <leti.ufmg@gmail.com>
2021-06-25 22:52:05 -0300

    Criando a classe SistemaUsuario

---
+++ include/gui/SistemaUsuario.hpp
@@ -0,0 +1,14 @@
+#ifndef __SISTEMAUSUARIO_H__
+#define __SISTEMAUSUARIO_H__
+#include <iostream>
+
+namespace GUI{
+    class SistemaUsuario{
+        public:
+            void show();
+
+        private:
+
+    };
+}
+#endif
