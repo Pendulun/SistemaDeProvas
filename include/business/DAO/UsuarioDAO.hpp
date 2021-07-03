9024c2f793bb8af29eb7b956c7c25a917ced8fa0
Pendulun <danielcampos14@hotmail.com.br>
2021-07-03 20:08:47 -0300

    Att UsuarioDAO

--- include/business/DAO/UsuarioDAO.hpp
+++ include/business/DAO/UsuarioDAO.hpp
@@ -1,11 +1,11 @@
 #ifndef __USUARIODAO_H__
 #define __USUARIODAO_H__
 #include <string>
+#include "business/DAO/BaseDAO.hpp"
 #include "business/Modelo/usuario.hpp"
 namespace Business{
-    class UsuarioDAO{
+    class UsuarioDAO : public BaseDAO<Modelo::Usuario>{
         public:
-            virtual void cadastrar(Modelo::Usuario) = 0;
             virtual bool login(std::string, std::string) = 0;
             virtual ~UsuarioDAO() = 0;
     };
