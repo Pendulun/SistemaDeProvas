2025d607fd16640d8020ca30ddc3d2fca85d77ff
Pendulun <danielcampos14@hotmail.com.br>
2021-07-04 00:07:51 -0300

    Novos métodos Aluno

--- src/business/Modelo/aluno.cpp
+++ src/business/Modelo/aluno.cpp
@@ -1,4 +1,29 @@
 #include "business/Modelo/aluno.hpp"
 
 namespace Modelo{
+    bool Aluno::isEmTurma(int idTurma){
+        for(Turma turma : this->turmas){
+            if(turma.getId()==idTurma){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void Aluno::adicionarTurma(Turma turma){
+        if(!this->isEmTurma(turma.getId())){
+            this->turmas.push_back(turma);
+        }
+    }
+
+    void Aluno::removerTurma(int id){
+        std::list<Turma>::iterator turmaIterator;
+        for(turmaIterator = this->turmas.begin(); turmaIterator != this->turmas.end(); turmaIterator++){
+            if(turmaIterator->getId() == id){
+                this->turmas.erase(turmaIterator);
+                break;
+            }
+        }
+    }
 }
