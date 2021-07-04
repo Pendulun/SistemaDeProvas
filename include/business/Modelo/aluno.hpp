09a2e39d1568f46864d68a347c729dc709355501
Pendulun <danielcampos14@hotmail.com.br>
2021-07-04 00:09:05 -0300

    Att Aluno HPP

--- include/business/Modelo/aluno.hpp
+++ include/business/Modelo/aluno.hpp
@@ -1,7 +1,9 @@
 #ifndef __ALUNO_H__
 #define __ALUNO_H__
+#include <list>
 #include "business/Modelo/usuario.hpp"
 #include "business/Modelo/tipoUsuario.hpp"
+#include "business/Modelo/turma.hpp"
 
 namespace Modelo{
     class Aluno : public Usuario{
@@ -9,6 +11,11 @@ namespace Modelo{
             Aluno(int id, TipoUsuario tipoUsuario) : Usuario(id, tipoUsuario){};
             Aluno(int id, TipoUsuario tipoUsuario, std::string nome, std::string login, std::string senha) : Usuario(id, tipoUsuario, nome, login, senha){};
             Aluno(int id, TipoUsuario tipoUsuario, std::string login, std::string senha) : Usuario(id, tipoUsuario, login, senha){};
+            bool isEmTurma(int);
+            void adicionarTurma(Modelo::Turma);
+            void removerTurma(int);
+        private:
+            std::list<Turma> turmas;
     };
 }
 #endif
