bdc37edbb08117307622ae0260f862c3572aa600
Pendulun <danielcampos14@hotmail.com.br>
2021-07-04 12:45:32 -0300

    Att BaseDAO
    Adicionei um método de pesquisa de vários elementos via uma lista de ids

--- include/business/DAO/BaseDAO.hpp
+++ include/business/DAO/BaseDAO.hpp
@@ -1,11 +1,13 @@
 #ifndef __BASEDAO_H__
 #define __BASEDAO_H__
 #include <string>
+#include <list>
 namespace Business{
     template <typename T> class BaseDAO{
         public:
             virtual bool cadastrar(T) = 0;
             virtual T* pesquisar(int) = 0;
+            virtual std::list<T*> pesquisar(const std::list<int>) = 0;
             virtual bool remover(T) = 0;
     };
 }
