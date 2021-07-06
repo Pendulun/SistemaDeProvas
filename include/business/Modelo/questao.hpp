e1c352c049d1ec89bae67bc931efb35f9dda7b09
Pendulun <danielcampos14@hotmail.com.br>
2021-07-05 22:57:23 -0300

    Usando namespace Modelo

--- include/business/Modelo/questao.hpp
+++ include/business/Modelo/questao.hpp
@@ -2,19 +2,20 @@
 #define QUESTAO_H
 #include <string>
 
-class Questao{
-    private:
-        std::string enunciado;
-        std::string textoAlternativas[4];
-        int alternativaCorreta;
-    public:
-        Questao(std::string, std::string, std::string, std::string, std::string, int);
-        std::string getEnunciado();
-        void setEnunciado(std::string);
-        std::string getTextoAlternativa(int);
-        void setTextoAlternativa(int,std::string);
-        int getAlternativaCorreta();
-        void setAlternativaCorreta(int);
-};
-
+namespace Modelo{
+    class Questao{
+        private:
+            std::string enunciado;
+            std::string textoAlternativas[4];
+            int alternativaCorreta;
+        public:
+            Questao(std::string, std::string, std::string, std::string, std::string, int);
+            std::string getEnunciado();
+            void setEnunciado(std::string);
+            std::string getTextoAlternativa(int);
+            void setTextoAlternativa(int,std::string);
+            int getAlternativaCorreta();
+            void setAlternativaCorreta(int);
+    };
+}
 #endif
--- src/business/Modelo/questao.cpp
+++ src/business/Modelo/questao.cpp
@@ -1,43 +1,45 @@
 #include "business/Modelo/questao.hpp"
-using namespace std;
 
-Questao::Questao(string enunciado,string alternativa1, string alternativa2, string alternativa3, string alternativa4,int alternativaCorreta){
-    this->enunciado=enunciado;
-    this->textoAlternativas[0]=alternativa1;
-    this->textoAlternativas[1]=alternativa2;
-    this->textoAlternativas[2]=alternativa3;
-    this->textoAlternativas[3]=alternativa4;
-    this->alternativaCorreta=alternativaCorreta;
-}
+namespace Modelo{
+    Questao::Questao(std::string enunciado, std::string alternativa1, std::string alternativa2, std::string alternativa3, std::string alternativa4, int alternativaCorreta){
+        this->enunciado=enunciado;
+        this->textoAlternativas[0]=alternativa1;
+        this->textoAlternativas[1]=alternativa2;
+        this->textoAlternativas[2]=alternativa3;
+        this->textoAlternativas[3]=alternativa4;
+        this->alternativaCorreta=alternativaCorreta;
+    }
 
-string Questao::getEnunciado(){
-    return this->enunciado;
-}
+    std::string Questao::getEnunciado(){
+        return this->enunciado;
+    }
 
-void Questao::setEnunciado(string enunciado){
-    this->enunciado=enunciado;
-}
+    void Questao::setEnunciado(std::string enunciado){
+        this->enunciado=enunciado;
+    }
 
-string Questao::getTextoAlternativa(int numero){
-    if(numero>0&&numero<=4){
-        return this->textoAlternativas[numero-1];
-    }else{
-        return NULL;
+    std::string Questao::getTextoAlternativa(int numero){
+        if(numero>0&&numero<=4){
+            return this->textoAlternativas[numero-1];
+        }else{
+            return NULL;
+        }
+    }
+
+    void Questao::setTextoAlternativa(int numero, std::string texto){
+        if(numero>0&&numero<=4){
+            this->textoAlternativas[numero-1]=texto;
+        }
+    }
+
+    int Questao::getAlternativaCorreta(){
+        return this->alternativaCorreta;
+    }
+
+    void Questao::setAlternativaCorreta(int numero){
+        if(numero>0&&numero<=4){
+            this->alternativaCorreta=numero;
+        }
     }
 }
 
-void Questao::setTextoAlternativa(int numero, string texto){
-    if(numero>0&&numero<=4){
-        this->textoAlternativas[numero-1]=texto;
-    }
-}
-
-int Questao::getAlternativaCorreta(){
-    return this->alternativaCorreta;
-}
-
-void Questao::setAlternativaCorreta(int numero){
-    if(numero>0&&numero<=4){
-        this->alternativaCorreta=numero;
-    }
-}
